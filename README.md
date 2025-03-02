289bfcecde2bb3f1f5b51718c3a6b34480b2103f
zhaozzo <3616184459@qq.com>
2025-03-02 14:53:24 +0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Stepper步骤条
+
+Stepper是一个自定义的WPF步骤条控件。
+
+## 背景
+
+基于Github上的开源项目Material Design Extensions的[Stepper](https://spiegelp.github.io/MaterialDesignExtensions/#documentation/stepper)抽取出来的，起因是要用到TabControlStepper这个控件，然后发现这个项目没有及时更新，因为这个项目是基于[Material Design in XAML Toolkit](https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit)的，然而Material Design已经更新到5.1.0了，其中一些名称和用法发生了改变，MDE中的Stepper无法在最新版的MD版本项目中使用了。于是就对其进行更新。
+
+## 使用方式
+
+在自己的项目中直接引用MaterialDesignSteppers.dll即可
+
+## 用法
+
+参考[Stepper](https://spiegelp.github.io/MaterialDesignExtensions/#documentation/stepper)，用法一模一样
+
+添加：
+
+StepButtonBar可以自定义字体大小
+
---
+++ Stepper/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stepper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new TabControlStepperViewModel();
+            Loaded += LoadedHandler;
+        }
+
+        private void LoadedHandler(object sender, RoutedEventArgs args)
+        {
+            // demo for setting a step via SelectedIndex property
+            //m_stepper.SelectedIndex = 1;
+        }
+
+        private void m_stepper_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
